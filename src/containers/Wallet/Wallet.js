880711da5075578fbc53575b4a15793d13b8447d
Brenden Smith <brcsmith12@gmail.com>
2019-02-18 20:25:16 -0600

    Added text to wallet page

--- src/containers/Wallet/Wallet.js
+++ src/containers/Wallet/Wallet.js
@@ -5,5 +5,10 @@ export default () => (
     <React.Fragment>
         <ButtonAppNav />
         <h1> Wallet page! </h1>
+        <h3>Balance</h3> 
+        <h4>
+            G3.56471031<br/>
+            $512.78
+        </h4>
     </React.Fragment>
 )
