32565226b69ff5f4e4000c08e796b6e4bb8b45d5
David Tavarez <david@punksec.com>
2020-05-01 00:08:41 +0200

    testing utils

---
+++ src/services/utils.test.ts
@@ -0,0 +1,44 @@
+import {
+  formatGrinAmount,
+  getFileExtension,
+  validateExtension,
+  validateUrl,
+  validateOnion,
+  validateAddress,
+} from "./utils";
+
+describe("Utils", () => {
+  test("formatGrinAmount()", () => {
+    expect(formatGrinAmount(10)).toBe(0.00000001);
+    expect(formatGrinAmount(20)).toBe(0.00000002);
+  });
+  test("getFileExtension()", () => {
+    expect(getFileExtension("0001.tx")).toBe("tx");
+    expect(getFileExtension("0001.response")).toBe("response");
+  });
+  test("validateExtension()", () => {
+    expect(validateExtension("0001.response", "response")).toBe(true);
+    expect(validateExtension("0001.response", "tx")).toBe(false);
+  });
+  test("validateUrl()", () => {
+    expect(validateUrl("http://duckduckgo.com")).toBe(true);
+    expect(validateUrl("https://duckduckgo.com")).toBe(true);
+    expect(validateUrl("http://duckduckgo.com/")).toBe(true);
+    expect(validateUrl("https://duckduckgo.com/")).toBe(true);
+    expect(validateUrl("http://duckduckgo:80")).toBe(false);
+    expect(validateUrl("http://duckduckgo.com:80")).toBe(true);
+    expect(validateUrl("https://duckduckgo.com:5000/")).toBe(true);
+  });
+  test("validateOnion()", () => {
+    expect(validateOnion("http://duckduckgo")).toBe(false);
+    expect(
+      validateOnion("jamie22ezawwi5r3o7lrgsno43jj7vq5en74czuw6wfmjzkhjjryxnid")
+    ).toBe(true);
+    expect(
+      validateOnion(
+        "http://jamie22ezawwi5r3o7lrgsno43jj7vq5en74czuw6wfmjzkhjjryxnid.onion"
+      )
+    ).toBe(true);
+  });
+  test("validateAddress()", () => {});
+});
