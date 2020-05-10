7a0bb6dffe1d0f12567507cdd37f426ff4ba2c92
David Tavarez <david@punksec.com>
2020-05-10 12:34:22 +0200

    Adding more translations (#31)

--- src/models/idiom.ts
+++ src/models/idiom.ts
@@ -1,18 +1,18 @@
 import { Action, action } from "easy-peasy";
-import ElectronStore from 'electron-store';
+import ElectronStore from "electron-store";
 
 export interface IdiomModel {
-  language: "ch" | "de" | "en" | "es" | "pr" | "ru" | "tr";
+  language: "ch" | "de" | "en" | "es" | "fr" | "pl" | "pr" | "ru" | "tr";
   setLanguage: Action<
     IdiomModel,
-    "ch" | "de" | "en" | "es" | "pr" | "ru" | "tr"
+    "ch" | "de" | "en" | "es" | "fr" | "pl" | "pr" | "ru" | "tr"
   >;
 }
 
 const AsyncStore = {
   electronStore: new ElectronStore<IdiomModel>(),
-  getItem(key:any, defaultValue: any) {
-    console.log('Getting item: ' + key);
+  getItem(key: any, defaultValue: any) {
+    console.log("Getting item: " + key);
     const value = this.electronStore.get(key);
     if (value == null) {
       return defaultValue;
@@ -20,21 +20,21 @@ const AsyncStore = {
 
     return value;
   },
-  setItem(key:any, data:any) {
-    console.log('Setting item: ' + key);
+  setItem(key: any, data: any) {
+    console.log("Setting item: " + key);
     console.log(data);
-    this.electronStore.set( key, data );
+    this.electronStore.set(key, data);
   },
-  removeItem(key:any) {
-    console.log('Removing item: ' + key);
-    this.electronStore.delete( key );
+  removeItem(key: any) {
+    console.log("Removing item: " + key);
+    this.electronStore.delete(key);
   },
 };
 
 const idiom: IdiomModel = {
-  language: AsyncStore.getItem('language', 'en'),
+  language: AsyncStore.getItem("language", "en"),
   setLanguage: action((state, language) => {
-    AsyncStore.setItem('language', language);
+    AsyncStore.setItem("language", language);
     state.language = language;
   }),
 };
