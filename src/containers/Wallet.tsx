ccdd80dd9d3221511bf83991e807387b2c08709b
David Tavarez <david@punksec.com>
2020-05-18 00:44:18 +0200

    fixing passwordprompt for seed backup

--- src/containers/Wallet.tsx
+++ src/containers/Wallet.tsx
@@ -1,40 +1,87 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useCallback } from "react";
 import { useStoreActions, useStoreState } from "../hooks";
 
 import { AlertComponent } from "../components/extras/Alert";
 import { LoadingComponent } from "../components/extras/Loading";
 import { Redirect } from "react-router-dom";
+import { PasswordPromptComponent } from "../components/wallet/open/PasswordPrompt";
+import { ISeed } from "../interfaces/ISeed";
+import { Toaster, Position, Intent, Alert } from "@blueprintjs/core";
+import { WalletSeedInputComponent } from "../components/shared/WalletSeedInput";
+import { useTranslation } from "react-i18next";
 
 const AccountNavBarContainer = React.lazy(() =>
-  import("./dashboard/AccountNavBar").then(module => ({
-    default: module.AccountNavBarContainer
+  import("./dashboard/AccountNavBar").then((module) => ({
+    default: module.AccountNavBarContainer,
   }))
 );
 
 const StatusBarContainer = React.lazy(() =>
-  import("./common/StatusBar").then(module => ({
-    default: module.StatusBarContainer
+  import("./common/StatusBar").then((module) => ({
+    default: module.StatusBarContainer,
   }))
 );
 
 const WalletDrawer = React.lazy(() =>
-  import("./common/WalletDrawer").then(module => ({
-    default: module.WalletDrawer
+  import("./common/WalletDrawer").then((module) => ({
+    default: module.WalletDrawer,
   }))
 );
 
 const DashboardContainer = React.lazy(() =>
-  import("./dashboard/Dashboard").then(module => ({
-    default: module.DashboardContainer
+  import("./dashboard/Dashboard").then((module) => ({
+    default: module.DashboardContainer,
   }))
 );
 
 const renderLoader = () => <LoadingComponent />;
 
 export const WalletContainer = () => {
-  const { isLoggedIn } = useStoreState(state => state.session);
-  const { alert } = useStoreState(state => state.ui);
-  const { setAlert } = useStoreActions(actions => actions.ui);
+  const { t } = useTranslation();
+
+  const { isLoggedIn, seed } = useStoreState((state) => state.session);
+  const { alert } = useStoreState((state) => state.ui);
+  const { status } = useStoreState((state) => state.nodeSummary);
+
+  const { username, password, waitingResponse } = useStoreState(
+    (state) => state.passwordPrompt
+  );
+  const { setUsername, setPassword, setWaitingResponse } = useStoreActions(
+    (state) => state.passwordPrompt
+  );
+  const { setAlert } = useStoreActions((actions) => actions.ui);
+  const { getWalletSeed, setSeed } = useStoreActions((state) => state.session);
+
+  const backupSeed = useCallback(async () => {
+    if (username === undefined || password === undefined) return;
+    setWaitingResponse(true);
+    try {
+      const seed: string[] = await getWalletSeed({
+        username: username,
+        password: password,
+      });
+      if (seed !== undefined && seed.length > 0) {
+        let _seed: ISeed[] = [];
+        for (let index = 0; index < seed.length; index++) {
+          const word = seed[index];
+          _seed.push({
+            position: index + 1,
+            text: word,
+            disabled: true,
+            valid: true,
+          });
+        }
+        setSeed(_seed);
+      }
+    } catch (error) {
+      Toaster.create({ position: Position.BOTTOM }).show({
+        message: error.message,
+        intent: Intent.DANGER,
+        icon: "warning-sign",
+      });
+    }
+    setWaitingResponse(false);
+  }, [getWalletSeed, username, password, setSeed, setWaitingResponse]);
 
   return (
     <Suspense fallback={renderLoader()}>
@@ -48,6 +95,46 @@ export const WalletContainer = () => {
       </div>
       <WalletDrawer />
       <AlertComponent message={alert} setMessage={setAlert} />
+      {isLoggedIn ? (
+        <PasswordPromptComponent
+          isOpen={username && username.length > 0 ? true : false}
+          username={username ? username : ""}
+          password={password ? password : ""}
+          passwordCb={(value: string) => setPassword(value)}
+          onCloseCb={() => {
+            setUsername(undefined);
+          }}
+          waitingResponse={waitingResponse}
+          passwordButtonCb={backupSeed}
+          connected={status.toLocaleLowerCase() !== "not connected"}
+          buttonText={t("confirm_password")}
+        />
+      ) : null}
+      {seed !== undefined ? (
+        <Alert
+          className="bp3-dark"
+          canEscapeKeyCancel={true}
+          canOutsideClickCancel={true}
+          onConfirm={() => {
+            setSeed(undefined);
+            setUsername(undefined);
+            setPassword(undefined);
+          }}
+          onCancel={() => {
+            setSeed(undefined);
+            setUsername(undefined);
+            setPassword(undefined);
+          }}
+          isOpen={seed !== undefined}
+          style={{ backgroundColor: "#050505" }}
+        >
+          <WalletSeedInputComponent
+            seed={seed}
+            onWordChangeCb={() => {}}
+            length={seed.length}
+          />
+        </Alert>
+      ) : null}
     </Suspense>
   );
 };
--- src/containers/common/Settings.tsx
+++ src/containers/common/Settings.tsx
@@ -1,16 +1,9 @@
-import { Alert, Intent, Position, Toaster } from "@blueprintjs/core";
 import React, { useCallback } from "react";
 import { useStoreActions, useStoreState } from "../../hooks";
 
-import { ISeed } from "../../interfaces/ISeed";
-import { PasswordPromptComponent } from "../../components/wallet/open/PasswordPrompt";
 import { SettingsComponent } from "../../components/extras/Settings";
-import { WalletSeedInputComponent } from "../../components/shared/WalletSeedInput";
-import { useTranslation } from "react-i18next";
 
 export const SettingsContainer = () => {
-  const { t } = useTranslation();
-
   const {
     mininumPeers,
     maximumPeers,
@@ -19,32 +12,27 @@ export const SettingsContainer = () => {
     nodeBinaryPath,
     useGrinJoin,
     grinJoinAddress,
-    isConfirmationDialogOpen
-  } = useStoreState(state => state.settings);
-  const { status } = useStoreState(state => state.nodeSummary);
-  const { floonet } = useStoreState(state => state.settings.defaultSettings);
-  const { isLoggedIn, username: sessionUsername, seed } = useStoreState(
-    state => state.session
-  );
-  const { username, password, waitingResponse } = useStoreState(
-    state => state.passwordPrompt
+    isConfirmationDialogOpen,
+  } = useStoreState((state) => state.settings);
+  const { status } = useStoreState((state) => state.nodeSummary);
+  const { floonet } = useStoreState((state) => state.settings.defaultSettings);
+  const { isLoggedIn, username: sessionUsername } = useStoreState(
+    (state) => state.session
   );
 
-  const { setUsername, setPassword, setWaitingResponse } = useStoreActions(
-    state => state.passwordPrompt
-  );
-  const { getWalletSeed, setSeed } = useStoreActions(state => state.session);
+  const { setUsername } = useStoreActions((state) => state.passwordPrompt);
   const {
     setMininumPeers,
     setMaximumPeers,
     setConfirmations,
     setGrinJoinUse,
     setGrinJoinAddress,
-    toggleConfirmationDialog
-  } = useStoreActions(actions => actions.settings);
+    toggleConfirmationDialog,
+  } = useStoreActions((actions) => actions.settings);
   const { reSyncBlockchain, restartNode } = useStoreActions(
-    state => state.wallet
+    (state) => state.wallet
   );
+  const { toggleSettings } = useStoreActions((actions) => actions.ui);
 
   const toggleDialog = useCallback(() => {
     toggleConfirmationDialog();
@@ -66,99 +54,31 @@ export const SettingsContainer = () => {
     await restartNode();
   }, [restartNode]);
 
-  const backupSeed = useCallback(async () => {
-    if (username === undefined || password === undefined) return;
-    setWaitingResponse(true);
-    try {
-      const seed: string[] = await getWalletSeed({
-        username: username,
-        password: password
-      });
-      if (seed !== undefined && seed.length > 0) {
-        let _seed: ISeed[] = [];
-        for (let index = 0; index < seed.length; index++) {
-          const word = seed[index];
-          _seed.push({
-            position: index + 1,
-            text: word,
-            disabled: true,
-            valid: true
-          });
-        }
-        setSeed(_seed);
-      }
-    } catch (error) {
-      Toaster.create({ position: Position.BOTTOM }).show({
-        message: error.message,
-        intent: Intent.DANGER,
-        icon: "warning-sign"
-      });
-    }
-    setWaitingResponse(false);
-  }, [getWalletSeed, username, password, setSeed, setWaitingResponse]);
-
   return (
-    <div>
-      <SettingsComponent
-        status={status}
-        floonet={floonet}
-        useGrinJoin={useGrinJoin}
-        grinJoinAddress={grinJoinAddress}
-        mininumPeers={mininumPeers}
-        maximumPeers={maximumPeers}
-        confirmations={confirmations}
-        nodeDataPath={nodeDataPath}
-        nodeBinaryPath={nodeBinaryPath}
-        isConfirmationDialogOpen={isConfirmationDialogOpen}
-        setGrinJoinUseCb={setGrinJoinUse}
-        setGrinJoinAddressCb={setGrinJoinAddress}
-        setMininumPeersCb={setMininumPeers}
-        setMaximumPeersCb={setMaximumPeers}
-        setConfirmationsCb={setConfirmations}
-        toggleConfirmationDialogCb={toggleDialog}
-        confirmReSyncBlockchainCb={confirmReSyncBlockchain}
-        restartNodeCb={restartGrinNode}
-        isLoggedIn={isLoggedIn}
-        backupButtonCb={() => setUsername(sessionUsername)}
-      />
-      {isLoggedIn ? (
-        <PasswordPromptComponent
-          isOpen={username && username.length > 0 ? true : false}
-          username={username ? username : ""}
-          password={password ? password : ""}
-          passwordCb={(value: string) => setPassword(value)}
-          onCloseCb={() => {}}
-          waitingResponse={waitingResponse}
-          passwordButtonCb={backupSeed}
-          connected={status.toLocaleLowerCase() !== "not connected"}
-          buttonText={t("confirm_password")}
-        />
-      ) : null}
-      {seed !== undefined ? (
-        <Alert
-          className="bp3-dark"
-          canEscapeKeyCancel={true}
-          canOutsideClickCancel={true}
-          onConfirm={() => {
-            setSeed(undefined);
-            setUsername(undefined);
-            setPassword(undefined);
-          }}
-          onCancel={() => {
-            setSeed(undefined);
-            setUsername(undefined);
-            setPassword(undefined);
-          }}
-          isOpen={seed !== undefined}
-          style={{ backgroundColor: "#050505" }}
-        >
-          <WalletSeedInputComponent
-            seed={seed}
-            onWordChangeCb={() => {}}
-            length={seed.length}
-          />
-        </Alert>
-      ) : null}
-    </div>
+    <SettingsComponent
+      status={status}
+      floonet={floonet}
+      useGrinJoin={useGrinJoin}
+      grinJoinAddress={grinJoinAddress}
+      mininumPeers={mininumPeers}
+      maximumPeers={maximumPeers}
+      confirmations={confirmations}
+      nodeDataPath={nodeDataPath}
+      nodeBinaryPath={nodeBinaryPath}
+      isConfirmationDialogOpen={isConfirmationDialogOpen}
+      setGrinJoinUseCb={setGrinJoinUse}
+      setGrinJoinAddressCb={setGrinJoinAddress}
+      setMininumPeersCb={setMininumPeers}
+      setMaximumPeersCb={setMaximumPeers}
+      setConfirmationsCb={setConfirmations}
+      toggleConfirmationDialogCb={toggleDialog}
+      confirmReSyncBlockchainCb={confirmReSyncBlockchain}
+      restartNodeCb={restartGrinNode}
+      isLoggedIn={isLoggedIn}
+      backupButtonCb={() => {
+        toggleSettings(false);
+        setUsername(sessionUsername);
+      }}
+    />
   );
 };
