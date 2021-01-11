d89b631fb052dc6c05461ffea9822b254c79ce5e
David Tavarez <david@punksec.com>
2021-01-11 09:56:37 +0100

    styling send max button

--- src/components/transaction/send/SpendableComponent.tsx
+++ src/components/transaction/send/SpendableComponent.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@blueprintjs/core";
+import { Button, Intent } from "@blueprintjs/core";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
@@ -26,9 +26,10 @@ export const SpendableComponent = ({
         style={{ marginLeft: "10px", marginRight: "10px" }}
       >{`${spendable.toFixed(9)} ツ`}</b>
       <Button
+        minimal={true}
         className="bp3-dark"
+        intent={Intent.PRIMARY}
         disabled={spendable > 0 ? false : true}
-        style={{ color: "black" }}
         onClick={onSendMaxButtonClickedCb}
       >
         {t("send_max")}
