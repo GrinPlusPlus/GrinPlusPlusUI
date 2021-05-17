4b87a1b20966b2c87e46e773ae3d768909b02d37
David Tavarez <david@punksec.com>
2021-05-17 18:43:16 +0200

    adding context menu to logs box

--- src/components/extras/TextFile.tsx
+++ src/components/extras/TextFile.tsx
@@ -1,10 +1,42 @@
 import React from "react";
 import { TextFileBox } from "../styled";
 
+import {
+  ContextMenu,
+  Menu,
+  MenuItem,
+  OverlayToaster,
+  Position,
+  Intent,
+} from "@blueprintjs/core";
+import { useTranslation } from "react-i18next";
+
 type TextFileComponentProps = {
   content: string;
 };
 
 export const TextFileComponent = ({ content }: TextFileComponentProps) => {
-  return <TextFileBox defaultValue={content}></TextFileBox>;
+  const { t } = useTranslation();
+
+  return (
+    <ContextMenu
+      className="bp4-dark"
+      content={
+        <Menu>
+          <MenuItem
+            text={t("copy_to_clipboard")}
+            onClick={() => {
+              navigator.clipboard.writeText(content);
+              OverlayToaster.create({ position: Position.BOTTOM }).show({
+                message: <div style={{ color: "white" }}>{t("copied")}</div>,
+                intent: Intent.SUCCESS,
+              });
+            }}
+          />
+        </Menu>
+      }
+    >
+      <TextFileBox defaultValue={content}></TextFileBox>
+    </ContextMenu>
+  );
 };
